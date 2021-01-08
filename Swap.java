ed8f194d7ba370c1894ed460e9c62ade3a1a0c51
Prabhavathi B <68562616+PrabhaBas@users.noreply.github.com>
2021-01-08 23:46:12 +0530

    Swapping

---
+++ Swap.java
@@ -0,0 +1,16 @@
+import java.io.*;
+class Swap
+{
+    public static void main(String arg[])throws IOException
+    {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter a,b:");
+        int a=Integer.parseInt(br.readLine());
+        int b=Integer.parseInt(br.readLine());
+        System.out.println("Before swapping= "+a+" "+b);
+        int c=a;
+        a=b;
+        b=c;
+        System.out.println("After swapping= "+a+" "+b);
+    }
+}
